2b820198a644db117fe480b49455318745adf665
César Gracián <cesar.gracian@icloud.com>
2024-05-02 08:45:47 -0600

    Se desactiva el mensaje de correo de recepción de pago predeterminado al cliente para usar otro más personalizado.

--- conekta_plugin.php
+++ conekta_plugin.php
@@ -56,7 +56,7 @@ class WC_Conekta_Plugin extends WC_Payment_Gateway
 		$title = sprintf("Se ha efectuado el pago del pedido %s", $order->get_order_number());
 		$body_message = "<p style=\"margin:0 0 16px\">Se ha detectado el pago del siguiente pedido:</p><br />" . $this->ckpg_assemble_email_payment($order);
 
-		// Email for customer
+		/*// Email for customer
 		$customer = esc_html($customer);
 		$customer = sanitize_text_field($customer);
 
@@ -66,7 +66,7 @@ class WC_Conekta_Plugin extends WC_Payment_Gateway
 			$body_message
 		);
 		$mail_customer->send($order->get_billing_email(), $title, $message);
-		unset($mail_customer);
+		unset($mail_customer);*/
 		//Email for admin site
 		$mail_admin = $woocommerce->mailer();
 		$message = $mail_admin->wrap_message(
